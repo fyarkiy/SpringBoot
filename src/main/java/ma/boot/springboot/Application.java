ca23bdfad718e27b206fe6153682d4f490183ffc
FYarkiy <flx_jb@yahoo.com>
2020-11-17 00:09:12 +0200

    add Application and ApplicationTest classes

---
+++ src/main/java/ma/boot/springboot/Application.java
@@ -0,0 +1,10 @@
+package ma.boot.springboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+    }
+}
